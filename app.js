898edb55c8b8ca19b940f375c461336fad514391
Anas Qazi <ianasqazi@gmail.com>
2019-12-07 16:34:01 -0500

    changed port number for heroku

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const shortid = require("shortid"); // library for creating a unique id
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
